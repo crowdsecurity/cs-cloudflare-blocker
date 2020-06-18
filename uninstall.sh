6078090967af786ccfa0c0d06cd0780287b912b3
AlteredCoder <AlteredCoder>
2020-06-18 15:33:35 +0200

    fix uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -4,11 +4,13 @@ BIN_PATH_INSTALLED="/usr/local/bin/cloudflare-blocker"
 CONFIG_DIR="/etc/crowdsec/cloudflare-blocker/"
 PID_DIR="/var/run/crowdsec/"
 SYSTEMD_PATH_FILE="/etc/systemd/system/cloudflare-blocker.service"
+LOG_FILE="/var/log/cloudflare-blocker.log"
 
 uninstall() {
 	systemctl stop cloudflare-blocker
 	rm -rf "${CONFIG_DIR}"
 	rm -f "${SYSTEMD_PATH_FILE}"
+	rm -f "${LOG_FILE}"
 	rm -f "${PID_DIR}cloudflare-blocker.pid"
 	rm -f "${BIN_PATH_INSTALLED}"
 }
